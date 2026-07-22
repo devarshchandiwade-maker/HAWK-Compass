f1d8f67a040c6f002fc805501576aa2ea1702dd1
devarshchandiwade-maker <devarsh.chandiwade@gozoop.in>
2026-07-22 16:07:27 +0530

    update email service 01

--- backend/utils/mailService.js
+++ backend/utils/mailService.js
@@ -3,14 +3,13 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // true only for port 465
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    tls: {
-        rejectUnauthorized: false,
-    },
+    family: 4,
 });
 
 // Verify SMTP connection when server starts
